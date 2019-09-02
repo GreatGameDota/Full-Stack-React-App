b8d453ebc0024cce9d322b452aab2efcfe51f059
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-09-02 10:19:10 -0500

    Add initial express functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,47 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+app.get('/posts', (req, res) => {
+	admin
+		.firestore()
+		.collection('posts')
+		.orderBy('createdAt', 'desc')
+		.get()
+		.then((data) => {
+			let posts = [];
+			data.forEach((doc) => {
+				posts.push({
+					postId: doc.id,
+					...doc.data()
+				});
+			});
+			return res.json(posts);
+		})
+		.catch((e) => {
+			console.error(e);
+		});
 });
+
+app.post('/posts', (req, res) => {
+	const newPost = {
+		body: req.body.body,
+		userHandle: req.body.userHandle,
+		createdAt: new Date().toISOString()
+	};
+	admin
+		.firestore()
+		.collection('posts')
+		.add(newPost)
+		.then((doc) => {
+			res.json({ message: `document ${doc.id} created successfully` });
+		})
+		.catch((err) => {
+			res.status(500).json({ error: 'Something went wrong' });
+			console.error(err);
+		});
+});
+
+exports.api = functions.https.onRequest(app);
