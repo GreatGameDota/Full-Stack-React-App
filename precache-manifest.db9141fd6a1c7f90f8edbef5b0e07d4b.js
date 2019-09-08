14b6f84f157da015446c4958d7bb95b6cbf15947
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-09-08 15:09:13 -0500

    Updates

---
+++ precache-manifest.db9141fd6a1c7f90f8edbef5b0e07d4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563cc764db57878c58ee42e12fdd700a",
+    "url": "/full-stack-react-app/index.html"
+  },
+  {
+    "revision": "bd659a769dfe7dcd4e2c",
+    "url": "/full-stack-react-app/static/css/main.c715a7f2.chunk.css"
+  },
+  {
+    "revision": "7297a671af9ff8e13532",
+    "url": "/full-stack-react-app/static/js/2.6757e0e0.chunk.js"
+  },
+  {
+    "revision": "bd659a769dfe7dcd4e2c",
+    "url": "/full-stack-react-app/static/js/main.36f00426.chunk.js"
+  },
+  {
+    "revision": "e17c8838c06fc4b52b21",
+    "url": "/full-stack-react-app/static/js/runtime~main.e9feee3d.js"
+  },
+  {
+    "revision": "f5ef4263a9c36adac59559031d67a0b7",
+    "url": "/full-stack-react-app/static/media/default-pp.f5ef4263.png"
+  },
+  {
+    "revision": "dd9994d129f83ae5ccbeb2c65ec2910f",
+    "url": "/full-stack-react-app/static/media/logo.dd9994d1.svg"
+  }
+]);
