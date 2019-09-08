51eb47097bd0f23df7cda2bd12e8b6bee314d25f
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-09-08 15:16:19 -0500

    Updates

---
+++ precache-manifest.b727266688055ca6eab9eee203a98bc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d82e9538e4d8c113f5b776eb271d47a",
+    "url": "/Full-Stack-React-App/index.html"
+  },
+  {
+    "revision": "7632bac7a63e543aee1e",
+    "url": "/Full-Stack-React-App/static/css/main.c715a7f2.chunk.css"
+  },
+  {
+    "revision": "7297a671af9ff8e13532",
+    "url": "/Full-Stack-React-App/static/js/2.6757e0e0.chunk.js"
+  },
+  {
+    "revision": "7632bac7a63e543aee1e",
+    "url": "/Full-Stack-React-App/static/js/main.45d6f994.chunk.js"
+  },
+  {
+    "revision": "9e9120e306573f408393",
+    "url": "/Full-Stack-React-App/static/js/runtime~main.a7c4e63f.js"
+  },
+  {
+    "revision": "f5ef4263a9c36adac59559031d67a0b7",
+    "url": "/Full-Stack-React-App/static/media/default-pp.f5ef4263.png"
+  },
+  {
+    "revision": "dd9994d129f83ae5ccbeb2c65ec2910f",
+    "url": "/Full-Stack-React-App/static/media/logo.dd9994d1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/full-stack-react-app/precache-manifest.db9141fd6a1c7f90f8edbef5b0e07d4b.js"
+  "/Full-Stack-React-App/precache-manifest.b727266688055ca6eab9eee203a98bc0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/full-stack-react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Full-Stack-React-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
