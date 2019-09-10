95a63c42a5f1f8113e55e9aa51d9e005a84aacb2
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-09-09 20:52:30 -0500

    Updates

--- precache-manifest.d882f92f0cf0133f713b7af4d275fdc2.js
+++ precache-manifest.d882f92f0cf0133f713b7af4d275fdc2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c76e40667c5c588729ea7d42f016208b",
+    "revision": "de2d755ab9204e23dd2404317ea22d0b",
     "url": "/Full-Stack-React-App/index.html"
   },
   {
-    "revision": "b911fd9b90cfcba8dc27",
+    "revision": "f86fe4060de21856edf2",
     "url": "/Full-Stack-React-App/static/css/main.c715a7f2.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Full-Stack-React-App/static/js/2.7b176cc8.chunk.js"
   },
   {
-    "revision": "b911fd9b90cfcba8dc27",
-    "url": "/Full-Stack-React-App/static/js/main.6d04d25b.chunk.js"
+    "revision": "f86fe4060de21856edf2",
+    "url": "/Full-Stack-React-App/static/js/main.767e1e5c.chunk.js"
   },
   {
     "revision": "9e9120e306573f408393",
